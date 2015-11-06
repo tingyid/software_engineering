d968af2294b2c8a205c3858f84cd535e47766e22
Dave Hand <davehand@users.noreply.github.com>
2015-11-05 21:16:12 -0500

    Added additional equivalence class, revised error messages

--- hw4/Flights-testMe/test/edu/upenn/cis573/flights/FlightFinderTest.java
+++ hw4/Flights-testMe/test/edu/upenn/cis573/flights/FlightFinderTest.java
@@ -54,16 +54,37 @@ public class FlightFinderTest {
 		// test a direct flight that should not exist
 		// based on what's in Flight.allFlights
 		
-		// note that, if "direct" is true, then "timeLimit" should not be used
 		int actual = ff.numFlights("PHL", "SEA", true, -100);
 		
 		int expected = 0;
+		assertEquals("Return value from numFlights incorrect when searching direct flight that doesn't exist", expected, actual);
+		
+		List<Flight> direct = ff.getDirectFlights();
+		assertEquals("Size of directFlights incorrect when searching direct flight that doesn't exist", 0, direct.size());
+			
+		// check that there are no indirect flights reported
+		List<Flight[]> indirect = ff.getIndirectFlights();
+		assertEquals("Size of indirectFlights incorrect when searching direct flight that doesn't exist", 0, indirect.size());
+	}
+	
+	@Test
+	public void testNumFlightsDirectFalseDirectFlightDoesExist() {
+		// test a direct flight that should exist with direct=false
+		// based on what's in Flight.allFlights
+		
+		int actual = ff.numFlights("PHL", "BOS", false, 5);
+		
+		int expected = 1;
 		assertEquals("Return value from numFlights incorrect when finding direct flights", expected, actual);
 		
 		// don't forget to check the side effects!!
 		List<Flight> direct = ff.getDirectFlights();
-		assertEquals("Size of directFlights incorrect when finding direct flights", 0, direct.size()); // check number of elements in list
-			
+		assertEquals("Size of directFlights incorrect when finding direct flights with direct as false", 1, direct.size()); 
+		
+		Flight f = direct.get(0);
+		assertEquals("Starting airport incorrect in first element when finding direct flights", "PHL", f.getStart());
+		assertEquals("Ending airport incorrect in first element when finding direct flights", "BOS", f.getEnd());
+		
 		// check that there are no indirect flights reported
 		List<Flight[]> indirect = ff.getIndirectFlights();
 		assertEquals("Size of indirectFlights incorrect when finding direct flights", 0, indirect.size());
@@ -77,17 +98,17 @@ public class FlightFinderTest {
 		int actual = ff.numFlights("PHL", "SEA", false, 600);
 		
 		int expected = 1;
-		assertEquals("Return value from numFlights incorrect when finding direct flights", expected, actual);
+		assertEquals("Return value from numFlights incorrect when finding indirect flights under time limit", expected, actual);
 		
 		List<Flight> direct = ff.getDirectFlights();
-		assertEquals("Size of directFlights incorrect when finding direct flights", 0, direct.size());
+		assertEquals("Size of directFlights incorrect when finding indirect flights under time limits", 0, direct.size());
 			
 		List<Flight[]> indirect = ff.getIndirectFlights();
-		assertEquals("Size of indirectFlights incorrect when finding direct flights", 1, indirect.size());
+		assertEquals("Size of indirectFlights incorrect when finding indirect flights under time limit", 1, indirect.size());
 		
 		Flight[] flights = indirect.get(0);
-		assertEquals("Starting airport incorrect in first element when finding direct flights", "PHL", flights[0].getStart());
-		assertEquals("Ending airport incorrect in first element when finding direct flights", "SEA", flights[1].getEnd());
+		assertEquals("Starting airport incorrect in first element when finding indirect flights", "PHL", flights[0].getStart());
+		assertEquals("Ending airport incorrect in first element when finding indirect flights", "SEA", flights[1].getEnd());
 	}
 	
 	@Test
@@ -98,13 +119,13 @@ public class FlightFinderTest {
 		int actual = ff.numFlights("PHL", "SEA", false, 500);
 		
 		int expected = 0;
-		assertEquals("Return value from numFlights incorrect when finding direct flights", expected, actual);
+		assertEquals("Return value from numFlights incorrect when finding indirect flights that exceed time limit", expected, actual);
 		
 		List<Flight> direct = ff.getDirectFlights();
-		assertEquals("Size of directFlights incorrect when finding direct flights", 0, direct.size());
+		assertEquals("Size of directFlights incorrect when finding indirect flights that exceed time limit", 0, direct.size());
 			
 		List<Flight[]> indirect = ff.getIndirectFlights();
-		assertEquals("Size of indirectFlights incorrect when finding direct flights", 0, indirect.size());
+		assertEquals("Size of indirectFlights incorrect when finding indirect flights that exceed time limit", 0, indirect.size());
 	}
 	
 	@Test
@@ -115,13 +136,13 @@ public class FlightFinderTest {
 		int actual = ff.numFlights("BOS", "SEA", false, 1000);
 		
 		int expected = 0;
-		assertEquals("Return value from numFlights incorrect when finding direct flights", expected, actual);
+		assertEquals("Return value from numFlights incorrect when finding indirect flights that do not exist", expected, actual);
 		
 		List<Flight> direct = ff.getDirectFlights();
-		assertEquals("Size of directFlights incorrect when finding direct flights", 0, direct.size());
+		assertEquals("Size of directFlights incorrect when finding indirect flights that do not exist", 0, direct.size());
 			
 		List<Flight[]> indirect = ff.getIndirectFlights();
-		assertEquals("Size of indirectFlights incorrect when finding direct flights", 0, indirect.size());
+		assertEquals("Size of indirectFlights incorrect when finding indirect flights that do not exist", 0, indirect.size());
 	}
 	
 	@Test
@@ -132,13 +153,13 @@ public class FlightFinderTest {
 		int actual = ff.numFlights("B", "SEA", false, 1000);
 		
 		int expected = -1;
-		assertEquals("Return value from numFlights incorrect when finding direct flights", expected, actual);
+		assertEquals("Return value from numFlights incorrect when home parameter invalid", expected, actual);
 		
 		List<Flight> direct = ff.getDirectFlights();
-		assertEquals("Size of directFlights incorrect when finding direct flights", 0, direct.size());
+		assertEquals("Size of directFlights incorrect when home parameter invalid", 0, direct.size());
 			
 		List<Flight[]> indirect = ff.getIndirectFlights();
-		assertEquals("Size of indirectFlights incorrect when finding direct flights", 0, indirect.size());
+		assertEquals("Size of indirectFlights incorrect when home parameter invalid", 0, indirect.size());
 	}
 	
 	@Test
@@ -149,13 +170,13 @@ public class FlightFinderTest {
 		int actual = ff.numFlights(null, "SEA", false, 1000);
 		
 		int expected = -1;
-		assertEquals("Return value from numFlights incorrect when finding direct flights", expected, actual);
+		assertEquals("Return value from numFlights incorrect when home parameter null", expected, actual);
 		
 		List<Flight> direct = ff.getDirectFlights();
-		assertEquals("Size of directFlights incorrect when finding direct flights", 0, direct.size());
+		assertEquals("Size of directFlights incorrect when home parameter null", 0, direct.size());
 			
 		List<Flight[]> indirect = ff.getIndirectFlights();
-		assertEquals("Size of indirectFlights incorrect when finding direct flights", 0, indirect.size());
+		assertEquals("Size of indirectFlights incorrect when home parameter null", 0, indirect.size());
 	}
 	
 	@Test
@@ -166,13 +187,13 @@ public class FlightFinderTest {
 		int actual = ff.numFlights("BOS", "S", false, 1000);
 		
 		int expected = -1;
-		assertEquals("Return value from numFlights incorrect when finding direct flights", expected, actual);
+		assertEquals("Return value from numFlights incorrect when dest parameter invalid", expected, actual);
 		
 		List<Flight> direct = ff.getDirectFlights();
-		assertEquals("Size of directFlights incorrect when finding direct flights", 0, direct.size());
+		assertEquals("Size of directFlights incorrect when dest parameter invalid", 0, direct.size());
 			
 		List<Flight[]> indirect = ff.getIndirectFlights();
-		assertEquals("Size of indirectFlights incorrect when finding direct flights", 0, indirect.size());
+		assertEquals("Size of indirectFlights incorrect when dest parameter invalid", 0, indirect.size());
 	}
 	
 	@Test
@@ -183,13 +204,13 @@ public class FlightFinderTest {
 		int actual = ff.numFlights("BOS", null, false, 1000);
 		
 		int expected = -1;
-		assertEquals("Return value from numFlights incorrect when finding direct flights", expected, actual);
+		assertEquals("Return value from numFlights incorrect when dest parameter null", expected, actual);
 		
 		List<Flight> direct = ff.getDirectFlights();
-		assertEquals("Size of directFlights incorrect when finding direct flights", 0, direct.size());
+		assertEquals("Size of directFlights incorrect when dest parameter null", 0, direct.size());
 			
 		List<Flight[]> indirect = ff.getIndirectFlights();
-		assertEquals("Size of indirectFlights incorrect when finding direct flights", 0, indirect.size());
+		assertEquals("Size of indirectFlights incorrect when dest parameter null", 0, indirect.size());
 	}
 	
 	@Test
@@ -199,13 +220,13 @@ public class FlightFinderTest {
 		int actual = ff.numFlights("PHL", "SEA", false, -1);
 		
 		int expected = -1;
-		assertEquals("Return value from numFlights incorrect when finding direct flights", expected, actual);
+		assertEquals("Return value from numFlights incorrect when finding indirect flights with invalid time limit", expected, actual);
 		
 		List<Flight> direct = ff.getDirectFlights();
-		assertEquals("Size of directFlights incorrect when finding direct flights", 0, direct.size());
+		assertEquals("Size of directFlights incorrect when finding indirect flights with invalid time limit", 0, direct.size());
 			
 		List<Flight[]> indirect = ff.getIndirectFlights();
-		assertEquals("Size of indirectFlights incorrect when finding direct flights", 0, indirect.size());
+		assertEquals("Size of indirectFlights incorrect when finding indirect flights with invalid time limit", 0, indirect.size());
 	}
 	
 	@Test
@@ -215,13 +236,13 @@ public class FlightFinderTest {
 		int actual = ff.numFlights("BOS", "BOS", false, 1000);
 		
 		int expected = -1;
-		assertEquals("Return value from numFlights incorrect when finding direct flights", expected, actual);
+		assertEquals("Return value from numFlights incorrect when home and dest are the same", expected, actual);
 		
 		List<Flight> direct = ff.getDirectFlights();
-		assertEquals("Size of directFlights incorrect when finding direct flights", 0, direct.size());
+		assertEquals("Size of directFlights incorrect when home and dest are the same", 0, direct.size());
 			
 		List<Flight[]> indirect = ff.getIndirectFlights();
-		assertEquals("Size of indirectFlights incorrect when finding direct flights", 0, indirect.size());
+		assertEquals("Size of indirectFlights incorrect when home and dest are the same", 0, indirect.size());
 	}
 
 }
